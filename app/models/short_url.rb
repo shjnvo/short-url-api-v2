a1683818f43aebf009c1bc56baeab3d99e57eead
VoDucLoi <shjnvo89@gmail.com>
2022-12-21 17:16:43 +0900

    assign code before validate in model ShortUrl

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,2 +1,17 @@
 class ShortUrl < ApplicationRecord
+  include Codeable
+
+  HOST = 'http://localhost:3000'
+
+  validates :original_url, :code, presence: true
+  validates :code, uniqueness: { case_sensitive: true }
+
+  before_validation :assign_code
+
+  private
+
+  def assign_code
+    generate_code(:code, 6) unless self.code
+    self.code
+  end
 end

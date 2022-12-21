45d6f4d8b53eed903d3ce2f4de2b295609b46d5f
VoDucLoi <shjnvo89@gmail.com>
2022-12-21 16:51:01 +0900

    make constraint for table short_urls

--- db/migrate/20221221070725_create_short_urls.rb
+++ db/migrate/20221221070725_create_short_urls.rb
@@ -1,12 +1,12 @@
 class CreateShortUrls < ActiveRecord::Migration[6.1]
   def change
     create_table :short_urls do |t|
-      t.string :original_url
-      t.string :code
+      t.string :original_url, null: false
+      t.string :code, null: false
 
       t.timestamps
     end
 
-    add_index :short_urls, :code
+    add_index :short_urls, :code, unique: true
   end
 end

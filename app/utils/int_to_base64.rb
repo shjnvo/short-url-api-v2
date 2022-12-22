168b4b8d231e694362c18cd7dd2b27960c2b3794
VoDucLoi <shjnvo89@gmail.com>
2022-12-22 10:52:21 +0900

    add utils IntToBase64

---
+++ app/utils/int_to_base64.rb
@@ -0,0 +1,16 @@
+class IntToBase64
+  BASE64_CHARACTERS = %w[
+    A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+    a b c d e f g h i j k l m n o p q r s t u v w x y z
+    0 1 2 3 4 5 6 7 8 9 - _
+  ].freeze
+
+  def self.encode(num)
+    result = ''
+    while num.positive?
+      result += BASE64_CHARACTERS[num % 64]
+      num /= 64
+    end
+    result
+  end
+end

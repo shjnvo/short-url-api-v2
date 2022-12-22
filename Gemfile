669b769ced1c7b312334949737d276cc6ea3f740
VoDucLoi <shjnvo89@gmail.com>
2022-12-22 12:44:05 +0900

    using enviroment variables for HOST

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.5'
 
+# A Ruby gem to load environment variables from `.env`
+gem 'dotenv-rails', groups: [:development, :test]
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.7'
 # Use postgresql as the database for Active Record
--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,10 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+if ['development', 'test'].include? ENV['RAILS_ENV']
+  Dotenv::Railtie.load
+end
+
 module UrlShortenerApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
